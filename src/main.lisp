8bbc01743ab3a08776eadc8e313e9d29f9386040
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2021-11-04 11:24:47 +0100

    feature: add a function
    the 'extended-euclid' function returns the greates common divisor of the
    arguments 'a' and 'b', as well as the values 'x' and 'y' which complete
    the equation:
    
      d = x*a + y*b

--- src/main.lisp
+++ src/main.lisp
@@ -1,5 +1,11 @@
 (ql:quickload :ltk)
 
+(defun extended-euclid (a b)
+  (if (= b 0)
+    (values a 1 0)
+    (multiple-value-bind (d x y) (extended-euclid b (mod a b))
+      (values d y (- x (* (floor a b) y))))))
+
 (defun main (*posix-argv*)
   ; gui main function
   (ltk:with-ltk ()
