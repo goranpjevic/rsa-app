d2146454762f46ff9b4fd9d25777d6f079ca0b39
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2021-11-05 14:36:33 +0100

    add comments
    some comments have been added that explain what the functions do.

--- src/main.lisp
+++ src/main.lisp
@@ -1,12 +1,16 @@
 (ql:quickload :ltk)
 
 (defun extended-euclid (a b)
+  ; return (d x y)
+  ; d=gcd(a b)
+  ; d=x*a+y*b
   (if (= b 0)
     (values a 1 0)
     (multiple-value-bind (d x y) (extended-euclid b (mod a b))
       (values d y (- x (* (floor a b) y))))))
 
 (defun modular-linear-equation-solver (a b n)
+  ; return solutions for x:
   ; a*x≡b(mod n)
   (multiple-value-bind (d x) (extended-euclid a n)
     (if (= (mod d b) 0)
