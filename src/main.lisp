102f6b21134fd083d90ab15154ed54fe987a836d
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2021-11-07 10:58:00 +0100

    refactor: fix indentation and a command binding
    some indentation has been changed to reduce unneeded whitespace.
    
    the 'generate-keys' function is called from withing the 'let' binding,
    which is now also 'let*', because the text of the 'number-of-bits-entry'
    is used as the function argument.

--- src/main.lisp
+++ src/main.lisp
@@ -98,25 +98,32 @@
 
 (defun main (*posix-argv*)
   ; gui main function
-  (ltk:with-ltk ()
-		; set window title
-		(ltk:wm-title ltk:*tk* "rsa")
-		; initialize gui widgets
-		(let ((generate-keys-button (make-instance 'ltk:button :text "generate keys"))
+  (ltk:with-ltk
+    ()
+    ; set window title
+    (ltk:wm-title ltk:*tk* "rsa")
+    ; initialize gui widgets
+    (let* ((number-of-bits-label (make-instance 'ltk:label :text "number of bits for keys"))
+	   (private-key-label (make-instance 'ltk:label :text "private key file"))
+	   (public-key-label (make-instance 'ltk:label :text "public key file"))
 
-		      (number-of-bits-label (make-instance 'ltk:label :text "number of bits for keys"))
-		      (private-key-label (make-instance 'ltk:label :text "private key file"))
-		      (public-key-label (make-instance 'ltk:label :text "public key file"))
+	   (number-of-bits-entry (make-instance 'ltk:entry))
+	   (private-key-entry (make-instance 'ltk:entry))
+	   (public-key-entry (make-instance 'ltk:entry))
 
-		      (number-of-bits-entry (make-instance 'ltk:entry))
-		      (private-key-entry (make-instance 'ltk:entry))
-		      (public-key-entry (make-instance 'ltk:entry)))
+	   (generate-keys-button
+	     (make-instance 'ltk:button
+			    :text "generate keys"
+			    :command
+			    (lambda ()
+			      (generate-keys
+				(read-from-string (ltk:text number-of-bits-entry)))))))
 
-		  (setf (ltk:command generate-keys-button) #'(lambda () (generate-keys 6)))
-
-		  ; put gui widgets on grid
-		  (ltk:grid private-key-label 0 0 :padx 5 :pady 5)
-		  (ltk:grid private-key-entry 0 1 :padx 5 :pady 5)
-		  (ltk:grid public-key-label 1 0 :padx 5 :pady 5)
-		  (ltk:grid public-key-entry 1 1 :padx 5 :pady 5)
-		  (ltk:grid generate-keys-button 2 0 :padx 5 :pady 5))))
+      ; put gui widgets on grid
+      (ltk:grid private-key-label 0 0 :padx 5 :pady 5)
+      (ltk:grid private-key-entry 0 1 :padx 5 :pady 5)
+      (ltk:grid public-key-label 1 0 :padx 5 :pady 5)
+      (ltk:grid public-key-entry 1 1 :padx 5 :pady 5)
+      (ltk:grid number-of-bits-label 2 0 :padx 5 :pady 5)
+      (ltk:grid number-of-bits-entry 2 1 :padx 5 :pady 5)
+      (ltk:grid generate-keys-button 3 0 :padx 5 :pady 5))))
