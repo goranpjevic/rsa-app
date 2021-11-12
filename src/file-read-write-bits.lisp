e19d4ab726a4e0523f733fe2268718c9c3cbe450
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2021-11-12 15:33:33 +0100

    split functions to a new file
    some functions have been moved to a new file, since they didn't work
    directly with reading or writing from or to files.

--- src/file-read-write-bits.lisp
+++ src/file-read-write-bits.lisp
@@ -1,31 +1,13 @@
 ; read and write a list bits from and to files
 
-(defun number-to-list-of-bits (byte-to-convert &optional (size 8))
-  ; convert a byte to a list of bits
-  (let ((bits '()))
-    (dotimes (index size bits)
-      (push (if (logbitp index byte-to-convert) 1 0) bits))))
-
 (defun read-list-of-bits-from-file (input-file-stream &optional (lst '()))
   ; return list of bits from the input file
   (let ((byte-read (read-byte input-file-stream nil)))
     (if byte-read
       (read-list-of-bits-from-file
-        input-file-stream
-        (append lst (number-to-list-of-bits byte-read)))
+	input-file-stream
+	(append lst (number-to-list-of-bits byte-read)))
       lst)))
-
-(defun list-of-bits-to-number (list-of-bits &optional (index 0) (number 0))
-  ; convert a list of bits to a number
-  (if (null list-of-bits)
-    number
-    (list-of-bits-to-number
-      (butlast list-of-bits)
-      (1+ index)
-      (+ number
-         (if (= 1 (car (last list-of-bits)))
-           (expt 2 index)
-           0)))))
 
 (defun write-bits-to-file (output-file-stream list-of-bits)
   ; output list of bits to an output file
@@ -40,17 +22,3 @@
 	(write-byte (list-of-bits-to-number (subseq new-list-of-bits 0 8))
 		    output-file-stream)
 	(write-bits-to-file output-file-stream (subseq new-list-of-bits 8))))))
-
-(defun split-list-by-n (list n)
-  ; split a list to a list of lists of length n
-  (do ((nn (1- n) (1- nn))
-       (part '())
-       (parts '()))
-    ((endp list)
-     (nreverse (if (endp part) parts
-		 (list* (nreverse part) parts))))
-    (push (pop list) part)
-    (when (zerop nn)
-      (push (nreverse part) parts)
-      (setf part '()
-	    nn n))))
---
+++ src/list-utils.lisp
@@ -0,0 +1,33 @@
+; functions used to work with lists
+
+(defun number-to-list-of-bits (byte-to-convert &optional (size 8))
+  ; convert a byte to a list of bits
+  (let ((bits '()))
+    (dotimes (index size bits)
+      (push (if (logbitp index byte-to-convert) 1 0) bits))))
+
+(defun list-of-bits-to-number (list-of-bits &optional (index 0) (number 0))
+  ; convert a list of bits to a number
+  (if (null list-of-bits)
+    number
+    (list-of-bits-to-number
+      (butlast list-of-bits)
+      (1+ index)
+      (+ number
+	 (if (= 1 (car (last list-of-bits)))
+	   (expt 2 index)
+	   0)))))
+
+(defun split-list-by-n (list n)
+  ; split a list to a list of lists of length n
+  (do ((nn (1- n) (1- nn))
+       (part '())
+       (parts '()))
+    ((endp list)
+     (nreverse (if (endp part) parts
+		 (list* (nreverse part) parts))))
+    (push (pop list) part)
+    (when (zerop nn)
+      (push (nreverse part) parts)
+      (setf part '()
+	    nn n))))
